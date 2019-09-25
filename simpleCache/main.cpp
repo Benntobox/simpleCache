574ebec37669001e2754e570b3932f07157fc6ff
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-09-24 20:43:30 -0700

    first steps

--- simpleCache/main.cpp
+++ simpleCache/main.cpp
@@ -7,6 +7,43 @@
 //
 
 #include <iostream>
+#include <map>
+using namespace std;
+
+class val {
+public:
+    int value;
+    val(int n) { value = n; }
+};
+
+class simpleCache {
+    map<int, int> mp;
+    int cp;
+    void set(int, val);
+    int get(int);
+};
+
+void simpleCache::set(int key, val value) {
+    if (mp.find(key) != mp.end()) {
+        if (mp.size() == cp) {
+            mp[0] = key;
+            
+        }
+        else {
+            mp.insert();
+        }
+    }
+}
+
+int simpleCache::get(int key) {
+    if(mp.find(key) != mp.end()) {
+        return mp[key];
+    }
+    else {
+        return -1;
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
