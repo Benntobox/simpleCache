c464aaf9272a422848b7c9afe41f0d3c1fbe41a4
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-09-24 20:33:47 -0700

    initialized

---
+++ simpleCache/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simpleCache
+//
+//  Created by Benjamin Boyle on 9/24/19.
+//  Copyright © 2019 Benjamin Boyle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
